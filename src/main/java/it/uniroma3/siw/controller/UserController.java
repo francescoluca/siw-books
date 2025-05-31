734cceb7fe0880ac7ca1b0efdabb07b71d99b696
francescoluca <francescoluca14@gmail.com>
2025-05-31 12:28:58 +0200

    Fix estetico login e book con bootstrap

--- src/main/java/it/uniroma3/siw/controller/UserController.java
+++ src/main/java/it/uniroma3/siw/controller/UserController.java
@@ -1,13 +1,18 @@
 package it.uniroma3.siw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import it.uniroma3.siw.model.Book;
 import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.model.User;
+import it.uniroma3.siw.service.BookService;
 import it.uniroma3.siw.service.CredentialsService;
 import it.uniroma3.siw.service.UserService;
 
@@ -18,6 +23,8 @@ public class UserController {
 	private UserService userService;
 	@Autowired
 	private CredentialsService credentialsService;
+	@Autowired
+	private BookService bookService;
 	
 	@GetMapping("/profile")
 	public String profile(Model model) {
@@ -26,5 +33,17 @@ public class UserController {
     	Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
 		model.addAttribute("user",userService.getUser(credentials.getId()));
 		return "/profile";
+	}
+	@GetMapping("/readBook/{bookId}")
+	public String readBook(@PathVariable ("bookId") Long bookId,Model model){
+		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    	Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
+    	Book book = bookService.findById(bookId);
+    	User currentUser = userService.getUser(credentials.getId());
+    	currentUser.getReadBooks().add(book);
+    	userService.saveUser(currentUser);
+    	model.addAttribute("user",currentUser);
+    	model.addAttribute("book",book);
+    	return "/book";
 	}
 }
